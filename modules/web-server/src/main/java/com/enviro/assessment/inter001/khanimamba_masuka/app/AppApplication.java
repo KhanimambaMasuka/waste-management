5d8d79f353a835bc42d5d9fd78d55c2c4607235d
KhanimambaMasuka <khanimambahm@gmail.com>
2024-04-18 22:54:12 +0200

    init

---
+++ modules/web-server/src/main/java/com/enviro/assessment/inter001/khanimamba_masuka/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.inter001.khanimamba_masuka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
---
+++ modules/web-server/src/test/java/com/enviro/assessment/inter001/khanimamba_masuka/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.inter001.khanimamba_masuka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
